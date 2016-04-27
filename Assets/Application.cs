0113d6d773bf1811d49ab5ad33190b228d4d6638
Victor Nogueira <felladrin@gmail.com>
2016-04-26 23:54:44 -0300

    Add Application.cs and MvcBehaviour.cs

---
+++ Assets/Application.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Application : MonoBehaviour
+{
+	public ModelContainer Model;
+	public ControllerContainer Controller;
+	public ViewContainer View;
+}
---
+++ Assets/MvcBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class MvcBehaviour : MonoBehaviour
+{
+	Application app;
+
+	protected Application App {
+		get {
+			if (app == null)
+				app = Object.FindObjectOfType<Application> ();
+
+			return app;
+		}
+	}
+}
