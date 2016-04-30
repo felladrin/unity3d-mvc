5c34538879027d11f64669d6d9b19153f11565e9
Victor Nogueira <felladrin@gmail.com>
2016-04-29 21:40:10 -0300

    Add GlobalStorage.cs
    This is the script used to hold global variables you want to make accessible through all scenes.

---
+++ Scripts/GlobalStorage.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class GlobalStorage : MonoBehaviour
+{
+	// Here you declare any other variables you
+	// want to make accessible through all scenes.
+	// For example: Health, Score, Kills.
+
+	#region Singleton definition
+	static GlobalStorage instance;
+
+	public static GlobalStorage Instance {
+		get { return instance; }
+	}
+
+	void GrantSingleInstance() {
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+	}
+	#endregion
+
+	void Awake ()
+	{
+		GrantSingleInstance ();
+		DontDestroyOnLoad (gameObject);
+	}
+}
